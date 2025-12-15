145354defda0268de5f724bc6bee660dfa0f0e79
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-12-15 12:54:47 +0500

    Update Footer component to include contact information and adjust layout styles

--- src/app/components/layout/Footer.js
+++ src/app/components/layout/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="container mx-auto">
         <div className="footer-top">
           <div className="flex flex-wrap">
-            <div className="col-lg-3 footer-brand order-1">
+            <div className="w-[40%]">
               <div className="image-wrapper mb-16">
                 <Image
                   src={
@@ -25,7 +25,7 @@ const Footer = () => {
 
               <p className="text-gray mb-50">Your Intelligent Transformation</p>
 
-              <div className="flex social-icons-wrapper">
+              <div className="flex w-[43.5%] social-icons-wrapper">
                 <div className="flex items-center gap-4 social-icons">
                   <Link
                     href="https://www.facebook.com/zaka4ai/"
@@ -102,6 +102,52 @@ const Footer = () => {
                 </div>
               </div>
             </div>
+            <div className="w-[20%]">
+              <div class="csn">
+                <h4>Email</h4>
+                <p>
+                  <a href="mailto:hello@zaka.ai" target="_blank">
+                    hello@zaka.ai
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div class="col-12 col-lg-3 footer-contact order-5 order-lg-3">
+              <div class="csn">
+                <h4>Email</h4>
+                <p>
+                  <Link href="mailto:hello@zaka.ai" target="_blank">
+                    hello@zaka.ai
+                  </Link>
+                </p>
+              </div>
+              <div class="csn">
+                <h4>Phone</h4>
+                <p>
+                  <Link href="tel:+96176855988" target="_blank">
+                    +96176855988
+                  </Link>{" "}
+                </p>
+              </div>
+              <div class="csn">
+                <h4>WhatsApp Number</h4>
+                <p>
+                  <Link href="https://wa.me/+12565680819" target="_blank">
+                    +12565680819
+                  </Link>
+                </p>
+              </div>
+              <div class="klpp">
+                <h4>Location</h4>
+                <p>
+                  651 N Broad St, Suite 206,
+                  <br />
+                  Middletown, New Castle, Delaware 19709
+                </p>
+              </div>
+            </div>
+            <div className="w-[20%]"></div>
+            <div className="w-[20%]"></div>
           </div>
         </div>
         <div className="footer-bottom">Footer Bottom</div>
