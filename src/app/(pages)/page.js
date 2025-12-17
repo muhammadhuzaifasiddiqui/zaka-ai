22294e0774816e2d05b5d10a53a60e5b2c145c06
muhammadhuzaifasiddiqui <huzaifasiddiquiep@gmail.com>
2025-12-17 09:26:24 +0500

    feat: Add contact form and update styles in page.js and globals.css

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -191,11 +191,82 @@ const page = () => {
       </div>
 
       {/* Contact-Section */}
-      <div className="social-media-section mt-100">
-        <div className="container mx-auto">
-          <div className="flex flex-row mb-16"></div>
+      <section className="contact-section mt-100">
+        <div className="container mx-auto rounded-[20px]">
+          <div className="contact-box flex flex-col w-full">
+            <form>
+              <h2>Do you have any questions?</h2>
+
+              <div className="flex flex-col">
+                <div className="flex flex-row gap-6">
+                  <div className="flex flex-col">
+                    <label htmlFor="username" className="mb-2.5">
+                      Full name
+                    </label>
+                    <input
+                      id="username"
+                      type="text"
+                      name="username"
+                      placeholder="John Smith"
+                    className="w-full mb-9 placeholder-[#E5E5E5] bg-[#404040] p-[20px] rounded-[8px] focus:outline-none"
+                    />
+                  </div>
+
+                  <div className="flex flex-col">
+                    <label htmlFor="email" className="mb-2.5">
+                      Your email
+                    </label>
+                    <input
+                      id="email"
+                      type="email"
+                      name="email"
+                      autoComplete="email"
+                      placeholder="Enter your email address"
+                    className="w-full mb-9 placeholder-[#E5E5E5] bg-[#404040] p-[20px] rounded-[8px] focus:outline-none"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="Subject" className="mb-2.5">
+                    Subject
+                  </label>
+                  <input
+                    id="Subject"
+                    type="text"
+                    name="Subject"
+                    placeholder="John Smith"
+                    className="w-full mb-9 placeholder-[#E5E5E5] bg-[#404040] p-[20px] rounded-[8px] focus:outline-none"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="Message" className="mb-2.5">
+                    Message
+                  </label>
+                  <textarea
+                    id="Message"
+                    name="Message"
+                    rows="3"
+                    placeholder="How can we assist you?"
+                    className="w-full mb-9 placeholder-[#E5E5E5] bg-[#404040] p-[20px] rounded-[8px] focus:outline-none"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="xl:mt-6 lg:mt-5 mt-4 flex">
+                <div className="flex group">
+                  <Link
+                    href="#"
+                    className="contact-btn"
+                  >
+                    SUBMIT
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      </section>
 
       <Footer />
     </>
